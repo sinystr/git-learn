a6bcb98dac1c3f8aad265a857cbddc0da276cd40
Georgi Nikolaev <sinystr@gmail.com>
2026-05-09 10:55:18 +0300

    iOS (#1)
    * Git learn
    
    * Git learn 1 added
    
    ---------
    
    Co-authored-by: Georgi Georgiev <ggeorgiev@gan.com>

---
+++ Git learn/Git learn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git learn
+//
+//  Created by Georgi Georgiev on 9.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!1")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git learn/Git learn/Git_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_learnApp.swift
+//  Git learn
+//
+//  Created by Georgi Georgiev on 9.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Git_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
